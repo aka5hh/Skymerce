ac9703449778d7f366c195cf8a8aef9839f58813
Abu Bakar Siddique <mohammad.akash31@gmail.com>
2023-12-06 15:51:29 +0600

    authentication for admin complete

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -6,9 +6,6 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller{
-    public function __construct(){
-        $this->middleware('auth');
-    }
     
     public function index(){
         return view('admin.dashboard');
--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
@@ -26,8 +27,16 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
-
+    //protected $redirectTo = RouteServiceProvider::HOME;
+    protected function authenticated(){
+        
+        if(Auth::user()->role_as == '1'){
+            return redirect('admin/dashboard')->with('message', 'Welcome back');
+        }
+        else{
+            return redirect('/home')->with('status', 'Logged In Successfully');
+        }
+    }
     /**
      * Create a new controller instance.
      *
